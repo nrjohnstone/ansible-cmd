09d3d6a1bfdf85acf7ebeeaefcff9279b3ee17f9
Nathan Johnstone <locotewz@gmail.com>
2023-10-30 15:26:08 +0100

    Update to ansible 8.5.0

--- build.ps1
+++ build.ps1
@@ -1,2 +1,2 @@
 docker build ./ansible-cmd/. -t "nrjohnstone/ansible-cmd:latest"
-docker build ./ansible-cmd/. -t "nrjohnstone/ansible-cmd:3.4.0"
+docker build ./ansible-cmd/. -t "nrjohnstone/ansible-cmd:8.5.0"
